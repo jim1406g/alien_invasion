e1e236b9a0382cc9debfe843a40b8a7681b820b1
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-12 18:07:34 +0600

    Make check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,6 @@ class AlienInvasion:
         """Инициализирует игру, создает игровые ресурсы."""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
@@ -21,17 +20,20 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
             # При каждом прохождении цикла, перерисовывается экран
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
 
 
 if __name__ == '__main__':
