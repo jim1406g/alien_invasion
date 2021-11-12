239e57438ce9ed17cd7883960ea47334840f761f
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-12 18:20:59 +0600

    Make update screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,24 +14,27 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
         self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
-            # При каждом прохождении цикла, перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Отображение последнего прорисованного экрана
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране, отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
+
 
 
 
