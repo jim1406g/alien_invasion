35dd975744854bc3672a750623d8b60dbfe1c640
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-12 18:23:20 +0600

    Change screen range for notebook

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ class Settings():
         """Инициализирует настройки игры."""
 
         # Параметры экрана
-        self.screen_width = 800
+        self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (0, 2, 54)
