24b0456d5ebb9cdcc4600e1c5f73e7ef9e7fc841
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-12 18:07:09 +0600

    Background color

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ class Settings():
         # Параметры экрана
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 2, 54)
