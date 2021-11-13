0ec103fa01957bb41f765429a0a765934abae68c
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-13 22:00:37 +0600

    Fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,19 +80,28 @@ class AlienInvasion:
     def _create_fleet(self):
         """Создает флот вторжения."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
+
+        # Количество пришельцев в ряду
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Количество рядов пришельцев
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         """Создание пришельца и размещение его в ряду."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
