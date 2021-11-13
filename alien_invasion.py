6e076188c59831077af2a6454b7fbb7c52eef706
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-11-13 21:49:33 +0600

    Fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,10 +85,15 @@ class AlienInvasion:
         number_aliens_x = available_space_x // (2 * alien_width)
 
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Создание пришельца и размещение его в ряду."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране, отображает новый экран."""
