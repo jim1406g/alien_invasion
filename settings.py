dc1453f49f3032e4f04fca32dd29aa04042b1a6d
Mikhail Zhikhalkin <mzhihalkin@vp.ru>
2021-12-18 17:44:24 +0600

    Score scale

--- settings.py
+++ settings.py
@@ -24,6 +24,8 @@ class Settings():
 
         # Темп ускорения игры
         self.speedup_scale = 1.5
+        # Темп увеличения стоимости пришельцев
+        self.score_scale = self.speedup_scale
 
         self.initialize_dynamic_settings()
 
@@ -40,3 +42,4 @@ class Settings():
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
